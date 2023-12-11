83441eb43253c04c82edd37e4dadb590d3bd7898
prajwalkotagi06 <prajwalkotagi2002@gmail.com>
2023-12-11 17:55:16 +0530

    'modify'

--- src/Components/Components/Tudoapp.jsx
+++ src/Components/Components/Tudoapp.jsx
@@ -1,21 +1,57 @@
 import React from 'react'
 import { Container, TextField, CssBaseline } from '@mui/material';
-import SearchIcon from '@mui/icons-material/Search';
-import mystyle from '@material-ui/core/styles'
+// import SearchIcon from '@mui/icons-material/Search';
+import { makeStyles } from '@material-ui/core/styles';
+// import { IconButton } from '@material-ui/core';
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
 
-const style = mystyle((theme) =>({
+const style = makeStyles((theme) =>({
     con:{
         marginTop: '200px',
+        border: '2px solid',
+        backgroundColor: 'white',
+        borderRadius: '10px',
+        height: '500px',
+    },
+    cont:{
+        width: '500px',
+        marginTop: '100px',
+        marginLeft: '200px',
+
+    },
+    te:{
+        width: '500px',
+
+    },
+    btn:{
+        backgroundColor: 'blue',
+        padding: '20px',
+        marginTop: '10px',
+        
+
     }
+
+
 }))
+
+  
+  
 function Tudoapp() {
     const classes = style();
+    
+      
   return (
     <>
         <CssBaseline>
-            <Container className='{classes. con}'>
-                <TextField id="outlined-basic" label="Outlined" variant="outlined" margin-top='200px' />
-                <SearchIcon />
+            <Container className={classes.con}>
+                <div className={classes.cont}>
+                    <TextField id="outlined-basic" label="Enter the list ***" variant="outlined"  className={classes.te}/>
+                    <Fab color="primary" aria-label="add">
+                        <AddIcon />
+                    </Fab>
+                </div>
+                
             </Container>
         </CssBaseline>
     </>
